55816b24f706b692fac2905ee52e75824d83b1ba
ManuelMaisano <manumaisano@gmail.com>
2024-03-10 21:23:17 +0100

    add variable js and consolelog

---
+++ script.js
@@ -0,0 +1,8 @@
+let km = parseInt ( prompt('Inserisci i kilometri da percorrere'));
+console.log(km);
+
+let age = parseInt(prompt('Inserisci la tua età'));
+console.log(age);
+
+let price = km * (0.21);
+console.log(price);
