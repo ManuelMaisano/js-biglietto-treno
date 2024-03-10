b11ac18e97dc29d0d78b42e761e14f7676f63e94
ManuelMaisano <manumaisano@gmail.com>
2024-03-10 21:41:22 +0100

    add toFixed js

--- script.js
+++ script.js
@@ -10,5 +10,11 @@ console.log(price);
 
 if(age < 18){
     price = price - ( price * 20 / 100);
-    document.getElementById('priceticket').innerHTML = 'Il prezzo del biglietto é :' + price
-} 
+    document.getElementById('priceticket').innerHTML = 'Il prezzo del biglietto é :' + price.toFixed(2)
+} else if (age >=65){
+    price = price - ( price * 40 / 100);
+    document.getElementById('priceticket').innerHTML = 'Il prezzo del biglietto é :' + price.toFixed(2)
+} else {
+    price = km * 0.21;
+    document.getElementById('priceticket').innerHTML = 'Il prezzo del biglietto é :' + price.toFixed(2)
+}
